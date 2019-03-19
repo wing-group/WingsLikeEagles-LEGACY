d024d9830b6be609625bdebede7eeecbeb1d6489
Dylan Dunn <dhd5076@rit.edu>
2019-03-19 14:54:03 -0400

    Finished basic user creation function

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -7,7 +7,14 @@ exports.list_users = function(req, res) {
 }
 
 exports.create_user = function(req, res) {
-    res.send('{ error: "NOT_IMPLEMENTED" }');
+    var user = new User({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password 
+        //TODO Need to add more fields to match schema and update API Docs
+    });
 }
 
 exports.delete_user = function(req, res) {
