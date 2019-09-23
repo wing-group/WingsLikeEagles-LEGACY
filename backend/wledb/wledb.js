8344d5562bd92de7d7692daf294839bb7e81197b
Jake O'Donnell <jakeod99@gmail.com>
2019-09-22 23:15:36 -0400

    wledb pool connection

--- backend/wledb/wledb.js
+++ backend/wledb/wledb.js
@@ -1,19 +1,35 @@
 // Copyright (c) 2019 WingGroup
 
+var util = require('util');
 var mysql = require('mysql');
 
-var wledb = mysql.createConnection({
+var wledb = mysql.createPool({
+    connectionLimit: 10,
     host: "wledb", // hostname is wledb, the mysql db is in the wledb service on our docker network
     user: "root",
     password: "", //TODO: Secure MySQL connection
     database: "wledb"
 });
 
-wledb.connect(function(err) {
-    if(err) {
-        throw err;
+wledb.getConnection(function(err, connection) {
+    if (err) {
+        if (err.code === 'PROTOCOL_CONNECTION_LOST') {
+            console.error('Database connection was closed.')
+        }
+        if (err.code === 'ER_CON_COUNT_ERROR') {
+            console.error('Database has too many connections.')
+        }
+        if (err.code === 'ECONNREFUSED') {
+            console.error('Database connection was refused.')
+        }
     }
-    console.log("Mysql connected")
+
+    if (connection) connection.release();
+
+    return
+
 });
+
+wledb.query = util.promisify(wledb.query);
 
 module.exports = wledb;
