0cec5f7d4e26d2e1f0fc39be1497bdd9f7858880
coryo12345 <corydio12@gmail.com>
2020-06-10 21:39:17 -0400

    switching translations

---
+++ frontend/src/js/Components/VerseDisplay/TranslationOverride.js
@@ -0,0 +1,59 @@
+import React, { Component } from 'react';
+
+export class TranslationOverride extends Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            bibles: []
+        };
+    }
+
+    /**
+     * When the DOM is rendered, get list of available bibles from server
+     */
+    componentDidMount() {
+        // // if user has already picked one, then dont reload the list
+        // if (this.props.selected !== -1) {
+        //     return;
+        // }
+
+        // fetch('/api/translations')
+        //     .then(resp => resp.json())
+        //     .then(data => {
+        //         // will need to test this json
+        //         this.setState({ bibles: data.content[0].bibles });
+        //     })
+        //     .catch(err => console.error(err));
+        this.setState({ bibles: ['KJV', 'NIV'] });
+        // this.props.callback('KJV');
+    }
+
+    /**
+     * send selected element to parent
+     */
+    change = (ev) => {
+        let s = ev.target;
+        let index = s.options[s.selectedIndex].value;
+        let data = this.state.bibles[index];
+        console.log(data);
+        this.props.changeTranslation(data);
+    }
+
+    render() {
+        // if there are none to pick from and user hasnt picked one yet
+        if (this.state.bibles.length === 0 && this.props.selected === -1) {
+            return (<div><select disabled></select></div>);
+        }
+        
+        return (
+            <div className="bg-gray-300 inline-block rounded p-1 pl-2 font-medium">
+                Translation: 
+                <select className="outline-none text-lg m-1 bg-gray-300 font-semibold" onChange={this.change}>
+                    {this.state.bibles && this.state.bibles.map((val, i) => {
+                        return <option className="text-lg" key={i} value={i} selected={this.props.selected === val}>{val}</option>
+                    })}
+                </select>
+            </div>
+        );
+    }
+}
--- frontend/src/js/Components/VerseDisplay/VerseDisplay.js
+++ frontend/src/js/Components/VerseDisplay/VerseDisplay.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react';
+import { TranslationOverride } from './TranslationOverride';
 
 export class VerseDisplay extends Component {
     constructor(props) {
         super(props);
-        this.state = { text: '' }
+        this.state = { text: '', translation: '' }
     }
 
     componentDidUpdate(prevProps) {
@@ -13,7 +14,7 @@ export class VerseDisplay extends Component {
                 .then(resp => resp.json())
                 .then(data => {
                     // will need to test this json
-                    this.setState({ text: data.content[0].book });
+                    this.setState({ text: data.content[0].book  + translation});
                 })
                 .catch(err => console.error(err));
         }
@@ -22,14 +23,30 @@ export class VerseDisplay extends Component {
     // Will look at a users profile to get the default translation...
     // for now we are assuming KJV
     getDefaultTranslation = () => {
+        this.setState({ translation: 'KJV' });
         return 'KJV';
     }
 
+    changeTranslation = (tr) => {
+        this.setState({ translation: tr, text: '' })
+
+        fetch(`/api/verses/${this.props.vid}?translation=${tr}`)
+            .then(resp => resp.json())
+            .then(data => {
+                // will need to test this json
+                this.setState({ text: data.content[0].book + tr, translation: tr });
+            })
+            .catch(err => console.error(err));
+    }
+
     render() {
-        if(this.props.vid === null) return null;
+        if (this.props.vid === null) return null;
         return (
             <div>
-                <p>{this.props.vid}</p>
+                <div className="flex">
+                    <TranslationOverride selected={this.state.translation} changeTranslation={this.changeTranslation} />
+                    <span className="inline-block font-bold text-2xl ml-3">{this.props.vid}</span>
+                </div>
                 <p>{this.state.text}</p>
             </div>
         )
