3b7ac6aa6e063d05c1397e50749831d2965d2847
Dylan Dunn <dylandunn17@icloud.com>
2020-06-12 17:44:47 -0400

    Start of new branch

--- backend/routes/api/me.js
+++ backend/routes/api/me.js
@@ -4,8 +4,8 @@
 
 var express = require('express');
 var router = express.Router();
-var userController = require('../controllers/userController');
-var authMiddleware = require('../middleware/auth');
+var userController = require('../../controllers/userController');
+var authMiddleware = require('../../middleware/auth');
 
 // GET /me/
 router.get('/', authMiddleware.requiresLogin, userController.get_current_user);
