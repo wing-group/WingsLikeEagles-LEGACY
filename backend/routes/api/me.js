e40d6c39448edb82feab294beafb548832cdbf85
Dylan Dunn <dylandunn17@icloud.com>
2020-06-14 15:22:00 -0400

    added routing for /me/password/

--- backend/routes/api/me.js
+++ backend/routes/api/me.js
@@ -5,12 +5,14 @@
 var express = require('express');
 var router = express.Router();
 var userController = require('../../controllers/userController');
-var authMiddleware = require('../../middleware/auth');
 
 // GET /me/
-router.get('/', authMiddleware.requiresLogin, userController.get_current_user);
+router.get('/', userController.get_current_user);
 
 // GET /me/tagged_verses
-router.get('/tagged_verses', authMiddleware.requiresLogin, usercontroller.get_tagged_verses);
+router.get('/tagged_verses', usercontroller.get_tagged_verses);
+
+// POST /me/password
+router.get('/password', userController.change_password);
 
 module.exports = router;
